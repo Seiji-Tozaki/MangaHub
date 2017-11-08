6e8fac310f7be791b0ef0033c200b48e7a0e0c14
Seiji-Tozaki <seiji.tozaki@gmail.com>
2017-11-09 00:31:59 +0900

    ファイル追加
    コミットやり直し

---
+++ MangaGitHub/src/main/java/com/ayamekiyama/mangagithub/MangaGitHubApplication.java
@@ -0,0 +1,12 @@
+package com.ayamekiyama.mangagithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaGitHubApplication.class, args);
+	}
+}
